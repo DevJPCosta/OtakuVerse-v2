fc0169874d5fda7472a725fb0d8335b7d23e4c95
m0narca <jppcsantosesola@gmail.com>
2023-06-22 09:03:49 -0300

    Revert "deu ruim"
    This reverts commit d0fe82d01fa69738bbdf30c5f31b0473813706fb.

--- app/src/main/java/com/example/myapp/FeedActivity.java
+++ app/src/main/java/com/example/myapp/FeedActivity.java
@@ -135,7 +135,7 @@ public class FeedActivity extends ListActivity {
     private static class ViewHolder {
         TextView textViewTitle;
         TextView textViewContent;
-        TextView textViewPostAuthor;
+        TextView textViewAuthor;
         TextView textViewDate;
         TextView textViewLikes;
         TextView textViewDislikes;
@@ -160,7 +160,7 @@ public class FeedActivity extends ListActivity {
                 holder = new ViewHolder();
                 holder.textViewTitle = convertView.findViewById(R.id.textViewTitle);
                 holder.textViewContent = convertView.findViewById(R.id.textViewContent);
-                holder.textViewPostAuthor = convertView.findViewById(R.id.textViewPostAuthor);
+                holder.textViewAuthor = convertView.findViewById(R.id.textViewAuthor);
                 holder.textViewDate = convertView.findViewById(R.id.textViewDate);
                 holder.textViewLikes = convertView.findViewById(R.id.textViewLikes);
                 holder.textViewDislikes = convertView.findViewById(R.id.textViewDislikes);
@@ -178,7 +178,7 @@ public class FeedActivity extends ListActivity {
             if (post != null) {
                 holder.textViewTitle.setText(post.getTitle());
                 holder.textViewContent.setText(post.getContent());
-                holder.textViewPostAuthor.setText(post.getAuthor());
+                holder.textViewAuthor.setText(post.getAuthor());
 
                 // Verifica se a data não é nula antes de chamá-la
                 if (post.getDate() != null) {
